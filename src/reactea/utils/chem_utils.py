8d82fff256101da1ec2dc52778948ed89616f0b4
João Correia <jfscorreia@ceb.uminho.pt>
2022-04-22 16:30:27 +0100

    [ADD] chemical interfaces for compounds and reaction rules implemented

--- src/reactea/utils/chem_utils.py
+++ src/reactea/utils/chem_utils.py
@@ -1,15 +1,26 @@
 from itertools import chain
+from typing import Union, List
 
 import numpy as np
 from rdkit import Chem, DataStructs
-from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import rdMolDescriptors, Mol, rdmolfiles, rdmolops
 
 
 class ChemUtils:
     """"""
 
     @staticmethod
-    def react(mol, rule):
+    def canonicalize_atoms(mol: Mol):
+        """"""
+        try:
+            new_order = rdmolfiles.CanonicalRankAtoms(mol)
+            mol = rdmolops.RenumberAtoms(mol, new_order)
+            return mol
+        except ValueError:
+            return mol
+
+    @staticmethod
+    def react(mol: Union[Mol, List[Mol]], rule):
         try:
             if not isinstance(mol, list):
                 products = rule.RunReactants((mol,))
@@ -17,21 +28,21 @@ class ChemUtils:
             else:
                 products = rule.RunReactants(tuple(mol))
                 return list(chain.from_iterable(products))
-        except Exception as e:
+        except ValueError:
             return ()
 
     @staticmethod
-    def atomPairsDescriptors(list_mols, nBits: int = 2048):
+    def atomPairsDescriptors(list_mols: List[Mol], n_bits: int = 2048):
         """"""
         invalids = np.zeros(len(list_mols), np.bool)
-        arr = np.zeros((len(list_mols), nBits))
+        arr = np.zeros((len(list_mols), n_bits))
 
         for i, mol in enumerate(list_mols):
             if mol is None:
                 invalids[i] = True
                 mol = Chem.MolFromSmiles("")
             res = np.zeros((1,))
-            desc = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(mol, nBits)
+            desc = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(mol, n_bits)
             DataStructs.ConvertToNumpyArray(desc, res)
             arr[i, :] = res
         return arr, invalids
