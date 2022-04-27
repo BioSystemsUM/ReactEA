ecba0c318d442e2252625c551c34c81dc9e3c558
João Correia <jfscorreia@ceb.uminho.pt>
2022-04-27 17:35:21 +0100

    [REFACTORING] only evaluate_solutions methods to evaluate solutions

--- src/reactea/optimization/problem.py
+++ src/reactea/optimization/problem.py
@@ -4,12 +4,27 @@ from typing import List, Union
 from reactea.chem.compounds import Compound
 from reactea.optimization.evaluation import ChemicalEvaluationFunction
 
+Num = Union[int, float]
+
 
 class Problem(ABC):
-    """"""
+    """
+    Base class for  representing a problem.
+    A problem its characterized by its name and by a list of Evaluation Functions.
+    Child classes must implement the evaluate_solutions method.
+    """
 
     def __init__(self, name: str, fevaluation: List[ChemicalEvaluationFunction]):
-        """"""
+        """
+        Initializes a problem.
+
+        Parameters
+        ----------
+        name: str
+            Name of the problem class.
+        fevaluation: List[ChemicalEvaluationFunction]
+            list of chemical evaluation functions.
+        """
         self.name = name
         if fevaluation is None:
             raise ValueError("At least one evaluation function needs to be provided")
@@ -19,57 +34,92 @@ class Problem(ABC):
 
     @property
     def is_maximization(self):
-        """"""
+        """
+        Checks if all evaluation functions are maximization problems.
+
+        Returns
+        -------
+        bool:
+            True if all evaluation functions are maximization functions. False otherwise.
+        """
         return all([f.maximize for f in self.fevaluation])
 
+    @abstractmethod
+    def evaluate_solutions(self, candidates: Union[Compound, List[Compound]]):
+        """
+        Evaluates a Compound or list of Compounds using the chemical evaluation functions.
+
+        Parameters
+        ----------
+        candidates: Union[Compound, List[Compound]]
+            List of Compounds to evaluate.
+
+        Returns
+        -------
+        Union[List[Num], List[List[Num]]:
+            fitness of the solutions for each evaluation function
+        """
+        raise NotImplementedError
+
     def __str__(self):
-        """"""
         return '{0} ({1} objectives)'.format(self.__class__.__name__, self.number_of_objectives)
 
     def __repr__(self):
-        """"""
         return self.__class__.__name__
-
-    def evaluate_solution(self, candidates: Union[Compound, List[Compound]], batched: bool):
-        """"""
-        return self._evaluate_solution_batch(candidates) if batched else self._evaluate_solution_single(candidates)
-
-    @abstractmethod
-    def _evaluate_solution_batch(self, candidates):
-        """"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def _evaluate_solution_single(self, candidates):
-        """"""
-        raise NotImplementedError
 
 
 class ChemicalProblem(Problem):
-    """"""
+    """
+    Class representing a Chemical Problem.
+    A Chemical Problem evaluates solutions represented as Compounds.
+    """
 
-    def __init__(self, fevaluation: List[ChemicalEvaluationFunction], configs: dict):
-        """"""
+    def __init__(self, fevaluation: List[ChemicalEvaluationFunction]):
+        """
+        Initializes a Chemical Problem.
+
+        Parameters
+        ----------
+        fevaluation: List[ChemicalEvaluationFunction]
+            list of chemical evaluation functions
+        """
         super(ChemicalProblem, self).__init__("ChemicalProblem", fevaluation)
-        self.configs = configs
 
-    def _evaluate_solution_batch(self, candidates: List[Compound]):
-        """"""
-        list_mols = [smi.mol for smi in candidates]
-        evals = []
-        for f in self.fevaluation:
-            evals.append(f(list_mols, batched=True))
-        return list(zip(*evals))
+    def evaluate_solutions(self, candidates: Union[Compound, List[Compound]]):
+        """
+        Evaluates Chemical Solutions using Chemical Evaluation Functions.
 
-    def _evaluate_solution_single(self, candidates: Compound):
-        """"""
-        candidates = candidates.mol
-        evals = []
-        for f in self.fevaluation:
-            evals.append(f(candidates, batched=False))
-        return evals
+        Parameters
+        ----------
+        candidates: Union[Compound, List[Compound]]
+            solutions to evaluate
 
-    def get_name(self):
-        """"""
+        Returns
+        -------
+        Union[List[Num], List[List[Num]]:
+            fitness of the solutions for each evaluation function.
+        """
+        if isinstance(candidates, list):
+            list_mols = [smi.mol for smi in candidates]
+            evals = []
+            for f in self.fevaluation:
+                evals.append(f(list_mols))
+            return list(zip(*evals))
+        else:
+            candidates = candidates.mol
+            evals = []
+            for f in self.fevaluation:
+                evals.append(f(candidates))
+            return evals
+
+    @staticmethod
+    def get_name():
+        """
+        Get the name of the problem class.
+
+        Returns
+        -------
+        str:
+            name of the Chemical Problem.
+        """
         return "ChemicalProblem"
-
