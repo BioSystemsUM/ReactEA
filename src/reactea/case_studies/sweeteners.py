a8bcb2418cd8092ef782c5004375374f36578c2c
João Correia <jfscorreia95@gmail.com>
2022-04-27 17:39:27 +0100

    [REFACTORING] parameters passed to __init__ instead of objective

--- src/reactea/case_studies/sweeteners.py
+++ src/reactea/case_studies/sweeteners.py
@@ -4,24 +4,64 @@ from reactea.optimization.problem import ChemicalProblem
 
 
 class SweetReactor(CaseStudy):
-    """"""
+    """
+    Sweeteners Case Study. Optimizes sweetener probability and probability of not being caloric.
+    """
 
-    def objective(self, configs: dict, multi_objective: bool = False):
-        """"""
-        f1 = SweetnessPredictionDeepSweet(configs)
+    def __init__(self, configs: dict, multi_objective: bool = False):
+        """
+        Initializes the sweet and non-caloric case study.
+
+        Parameters
+        ----------
+        configs : dict
+            dictionary with the experiment configurations.
+        multi_objective: bool
+            boolean defining if we are facing a single or multi-objective optimization problem.
+        """
+        super(SweetReactor, self).__init__(configs, multi_objective)
+        self.configs = configs
+        self.multi_objective = multi_objective
+
+    def objective(self):
+        """
+        Defines the evaluation functions to use in the optimization problem (sweetness and non-caloric) taking into
+        account if we are facing a single or multi-objective problem.
+
+        Returns
+        -------
+        Problem
+            ChemicalProblem object defining the evaluation functions, SweetnessPredictionDeepSweet and Caloric,
+            of this optimization problem.
+        """
+        f1 = SweetnessPredictionDeepSweet(self.configs)
         f2 = Caloric()
-        if multi_objective:
-            problem = ChemicalProblem([f1, f2], configs)
+        if self.multi_objective:
+            problem = ChemicalProblem([f1, f2], self.configs)
             return problem
         else:
-            f3 = AggregatedSum([f1, f2], [0.7, 0.3])
-            problem = ChemicalProblem([f3], configs)
+            f3 = AggregatedSum([f1, f2], [0.5, 0.5])
+            problem = ChemicalProblem([f3], self.configs)
             return problem
 
     def name(self):
-        """"""
+        """
+        Defines the name of this Case Study.
+
+        Returns
+        -------
+        str
+            Name of this case study.
+        """
         return f"SweetenersReactor"
 
     def feval_names(self):
-        """"""
-        return f"probSweet;caloric"
+        """
+        Defines the names of the evaluation functions used in this Case Study.
+
+        Returns
+        -------
+        str
+            Name of the evaluation functions used in this case study.
+        """
+        return f"probSweet;caloric" if self.multi_objective else f"probSweet-caloric"
