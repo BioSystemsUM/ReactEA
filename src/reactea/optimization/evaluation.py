7bbc3388ef34457b12ecf0cb85c499a13c186ae9
João Correia <jfscorreia95@gmail.com>
2022-04-27 17:31:53 +0100

    [REFACTORING] only get_fitness method to evaluate solutions

--- src/reactea/optimization/evaluation.py
+++ src/reactea/optimization/evaluation.py
@@ -3,12 +3,10 @@ from functools import reduce
 from typing import List, Union
 
 import numpy as np
-from rdkit import DataStructs
-from rdkit.Chem import MolFromSmarts, Mol, rdMolDescriptors, MolToSmiles
+from rdkit.Chem import MolFromSmarts, Mol
 from rdkit.Chem.Crippen import MolLogP
 from rdkit.Chem.QED import qed
 
-from reactea.utilities.chem_utils import ChemUtils
 from reactea.utilities.io import Loaders
 
 
@@ -20,32 +18,23 @@ class ChemicalEvaluationFunction(ABC):
         self.maximize = maximize
         self.worst_fitness = worst_fitness
 
-    def get_fitness(self, candidate: Mol, batched: bool):
-        """"""
-        return self._get_fitness_batch(candidate) if batched else self._get_fitness_single(candidate)
-
     @abstractmethod
-    def _get_fitness_single(self, candidate: Mol):
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
         """"""
-        return NotImplementedError
-
-    @abstractmethod
-    def _get_fitness_batch(self, list_candidates: List[Mol]):
-        """"""
-        return NotImplementedError
+        raise NotImplementedError
 
     @abstractmethod
     def method_str(self):
         """"""
-        return NotImplementedError
+        raise NotImplementedError
 
     def __str__(self):
         """"""
         return self.method_str()
 
-    def __call__(self, candidate: Union[Mol, List[Mol]], batched: bool):
+    def __call__(self, candidate: Union[Mol, List[Mol]]):
         """"""
-        return self.get_fitness(candidate, batched)
+        return self.get_fitness(candidate)
 
 
 class DummyEvalFunction(ChemicalEvaluationFunction):
@@ -55,13 +44,12 @@ class DummyEvalFunction(ChemicalEvaluationFunction):
         """"""
         super(DummyEvalFunction, self).__init__(maximize, worst_fitness)
 
-    def _get_fitness_single(self, candidate: Mol):
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
         """"""
-        return len(candidate)
-
-    def _get_fitness_batch(self, list_candidates: List[Mol]):
-        """"""
-        return [len(candidate) for candidate in list_candidates]
+        if isinstance(candidates, list):
+            return [len(candidate) for candidate in candidates]
+        else:
+            return len(candidates)
 
     def method_str(self):
         """"""
@@ -84,21 +72,20 @@ class AggregatedSum(ChemicalEvaluationFunction):
         else:
             self.tradeoffs = np.array([1 / len(self.fevaluation)] * (len(self.fevaluation)))
 
-    def _get_fitness_single(self, candidate):
+    def get_fitness(self, candidates):
         """"""
-        res = []
-        for f in self.fevaluation:
-            res.append(f._get_fitness_single(candidate))
-        return np.dot(res, self.tradeoffs)
-
-    def _get_fitness_batch(self, list_mols):
-        """"""
-        evals = []
-        for f in self.fevaluation:
-            evals.append(f._get_fitness_batch(list_mols))
-        evals = np.transpose(np.array(evals))
-        res = np.dot(evals, self.tradeoffs)
-        return res
+        if isinstance(candidates, list):
+            res = []
+            for f in self.fevaluation:
+                res.append(f.get_fitness(candidates))
+            return np.dot(res, self.tradeoffs)
+        else:
+            evals = []
+            for f in self.fevaluation:
+                evals.append(f.get_fitness(candidates))
+            evals = np.transpose(np.array(evals))
+            res = np.dot(evals, self.tradeoffs)
+            return res
 
     def method_str(self):
         return "Aggregated Sum = " + reduce(lambda a, b: a + " " + b, [f.method_str() for f in self.fevaluation], "")
@@ -112,67 +99,25 @@ class SweetnessPredictionDeepSweet(ChemicalEvaluationFunction):
         self.configs = configs
         self.ensemble = Loaders.load_deepsweet_ensemble()
 
-    def _get_fitness_single(self, candidate: Mol):
-        """"""
+    def _predict_sweet_prob(self, mol: Mol):
         try:
-            res, _ = self.ensemble.predict([candidate])[0]
-        except ValueError:
-            res = self.worst_fitness
+            res, _ = self.ensemble.predict([mol])
+        except Exception:
+            res = [self.worst_fitness]
         return res
 
-    def _get_fitness_batch(self, list_candidates: List[Mol]):
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
         """"""
-        scores = []
-        for mol in list_candidates:
-            try:
-                res, _ = self.ensemble.predict([mol])
-            except ValueError:
-                res = [self.worst_fitness]
-            scores.extend(res)
-        return scores
+        if isinstance(candidates, list):
+            scores = []
+            for mol in candidates:
+                scores.extend(self._predict_sweet_prob(mol))
+            return scores
+        else:
+            return self._predict_sweet_prob(candidates)
 
     def method_str(self):
         return "Sweetness Prediction (DeepSweet)"
-
-
-class SweetnessPrediction(ChemicalEvaluationFunction):
-    """"""
-
-    def __init__(self, configs, maximize=True, worst_fitness=0.0):
-        """"""
-        super(SweetnessPrediction, self).__init__(maximize, worst_fitness)
-        self.config = configs
-        self.m1, self.m2, self.m3 = Loaders.loadSweetModels(self.config)
-
-    def _get_fitness_single(self, candidate):
-        """"""
-        if candidate:
-            fps = np.zeros((1,))
-            desc = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(candidate, 2048)
-            DataStructs.ConvertToNumpyArray(desc, fps)
-            fps = np.expand_dims(fps, axis=0)
-            score1 = self.m1.predict_proba(fps)[0, 1]
-            score2 = self.m2.predict_proba(fps)[0, 1]
-            score3 = self.m3.predict_proba(fps)[0, 0]
-            res = score1 + score2 + score3
-            res = res / 3
-        else:
-            res = self.worst_fitness
-        return res
-
-    def _get_fitness_batch(self, list_mols):
-        """"""
-        atomPairsArray, invalids = ChemUtils.atomPairsDescriptors(list_mols)
-        scores = np.zeros((len(list_mols), 3))
-        scores[:, 0] = self.m1.predict_proba(atomPairsArray)[:, 1]
-        scores[:, 1] = self.m2.predict_proba(atomPairsArray)[:, 1]
-        scores[:, 2] = self.m3.predict(atomPairsArray)[:, 0]
-        scores = np.sum(scores, axis=1) / 3.0
-        scores[invalids] = self.worst_fitness
-        return scores
-
-    def method_str(self):
-        return "Sweetness Prediction"
 
 
 class Caloric(ChemicalEvaluationFunction):
@@ -182,34 +127,23 @@ class Caloric(ChemicalEvaluationFunction):
         """"""
         super(Caloric, self).__init__(maximize=maximize, worst_fitness=worst_fitness)
 
-    def _get_fitness_single(self, candidate: Mol):
-        """"""
-        if candidate:
-            try :
-                caloric_smarts = MolFromSmarts("[Or5,Or6,Or7,Or8,Or9,Or10,Or11,Or12]")
-                n_matchs = len(candidate.GetSubstructMatches(caloric_smarts))
-                score = 1 / (n_matchs*3 + 1)
-            except Exception:
-                score = self.worst_fitness
-        else:
+    def _match_score(self, candidate: Mol, penalty_ratio: int = 3):
+        try:
+            caloric_smarts = MolFromSmarts("[Or5,Or6,Or7,Or8,Or9,Or10,Or11,Or12]")
+            n_matches = len(candidate.GetSubstructMatches(caloric_smarts))
+            score = 1 / (n_matches * penalty_ratio + 1)
+        except Exception:
             score = self.worst_fitness
         return score
 
-    def _get_fitness_batch(self, list_mols: List[Mol]):
-        """"""
-        listScore = []
-        for mol in list_mols:
-            if mol:
-                try:
-                    caloric_smarts = MolFromSmarts("[Or5,Or6,Or7,Or8,Or9,Or10,Or11,Or12]")
-                    n_matchs = len(mol.GetSubstructMatches(caloric_smarts))
-                    score = 1 / (n_matchs*3 + 1)
-                    listScore.append(score)
-                except Exception:
-                    listScore.append(self.worst_fitness)
-            else:
-                listScore.append(self.worst_fitness)
-        return listScore
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
+        if isinstance(candidates, list):
+            scores = []
+            for mol in candidates:
+                scores.append(self._match_score(mol))
+            return scores
+        else:
+            return self._match_score(candidates)
 
     def method_str(self):
         return "Caloric"
@@ -221,23 +155,21 @@ class LogP(ChemicalEvaluationFunction):
     def __init__(self, maximize=True, worst_fitness=-100.0):
         super(LogP, self).__init__(maximize=maximize, worst_fitness=worst_fitness)
 
-    def _get_fitness_single(self, candidate: Mol):
-        """"""
-        if candidate:
-            score = MolLogP(candidate)
-        else:
+    def _get_logp(self, mol: Mol):
+        try:
+            score = MolLogP(mol)
+        except Exception:
             score = self.worst_fitness
         return score
 
-    def _get_fitness_batch(self, list_mols: List[Mol]):
-        """"""
-        list_scores = []
-        for mol in list_mols:
-            try:
-                list_scores.append(MolLogP(mol))
-            except Exception:
-                list_scores.append(self.worst_fitness)
-        return list_scores
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
+        if isinstance(candidates, list):
+            scores = []
+            for mol in candidates:
+                scores.append(self._get_logp(mol))
+            return scores
+        else:
+            return self._get_logp(candidates)
 
     def method_str(self):
         return "logP"
@@ -249,23 +181,21 @@ class QED(ChemicalEvaluationFunction):
     def __init__(self, maximize=True, worst_fitness=-10.0):
         super(QED, self).__init__(maximize, worst_fitness)
 
-    def _get_fitness_single(self, candidate: Mol):
-        """"""
-        if candidate:
-            score = qed(candidate)
-        else:
+    def _qed(self, mol: Mol):
+        try:
+            score = qed(mol)
+        except Exception:
             score = self.worst_fitness
         return score
 
-    def _get_fitness_batch(self, list_mols):
-        """"""
-        list_scores = []
-        for mol in list_mols:
-            try:
-                list_scores.append(qed(mol))
-            except Exception:
-                list_scores.append(self.worst_fitness)
-        return list_scores
+    def get_fitness(self, candidates: Union[Mol, List[Mol]]):
+        if isinstance(candidates, list):
+            scores = []
+            for mol in candidates:
+                scores.append(self._qed(mol))
+            return scores
+        else:
+            return self._qed(candidates)
 
     def method_str(self):
         return "QED"
