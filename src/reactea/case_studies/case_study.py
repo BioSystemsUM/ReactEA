124db17c6094644dad3a6917a2b443780ac75879
João Correia <jfscorreia95@gmail.com>
2022-04-27 17:27:54 +0100

    [REFACTORING] params passed through __init__ instead of objective

--- src/reactea/case_studies/case_study.py
+++ src/reactea/case_studies/case_study.py
@@ -1,25 +1,59 @@
-import copy
 from abc import ABC, abstractmethod
 
 
 class CaseStudy(ABC):
-    """"""
+    """
+    Base class for all Case Studies.
+    A Case Study defines the evaluation functions to use in the optimization problem.
+    """
+
+    def __init__(self, configs: dict, multi_objective: bool = False):
+        """
+        Initializes the case study at a class level.
+
+        Parameters
+        ----------
+         configs : dict
+            dictionary with the experiment configurations.
+        multi_objective: bool
+            boolean defining if we are facing a single or multi-objective optimization problem.
+        """
+        self.configs = configs
+        self.multi_objective = multi_objective
 
     @abstractmethod
-    def objective(self, configs: dict, multiObjective: bool = False):
-        """"""
+    def objective(self):
+        """
+        Defines the evaluation functions to use in the optimization problem taking into account if we are facing a
+        single or multi-objective problem.
+
+        Returns
+        -------
+        Problem
+            Problem object defining the evaluation functions of the optimization problem.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def name(self):
-        """"""
+        """
+        Defines the name of the Case Study.
+
+        Returns
+        -------
+        str
+            Name of the case study.
+        """
         return NotImplementedError
 
     @abstractmethod
     def feval_names(self):
-        """"""
-        return NotImplementedError
+        """
+        Defines the names of the evaluation functions used in the Case Study.
 
-    def __str__(self):
-        """"""
-        return self.name()
+        Returns
+        -------
+        str
+            Name of the evaluation functions used in the case study.
+        """
+        return NotImplementedError
