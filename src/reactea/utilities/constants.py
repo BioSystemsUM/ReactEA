4ca0ab00d8a706630ad4f1b5ec314683258c4b76
João Correia <jfscorreia95@gmail.com>
2022-05-04 15:39:43 +0100

    [ADD] add more SO and MO algorithms constants

--- src/reactea/utilities/constants.py
+++ src/reactea/utilities/constants.py
@@ -1,5 +1,6 @@
 from jmetal.algorithm.multiobjective.nsgaiii import UniformReferenceDirectionFactory
 from jmetal.operator import BinaryTournamentSelection
+from jmetal.util.comparator import DominanceComparator
 from jmetal.util.termination_criterion import StoppingByEvaluations
 
 from reactea.chem.standardization import ChEMBLStandardizer
@@ -10,7 +11,6 @@ class EAConstants:
     """
     Class containing a set of EA parameters constants
     """
-
     # Maximum number of generations (used as stopping criteria for the EA)
     MAX_GENERATIONS = 100
     # Multiprocessing
@@ -21,15 +21,20 @@ class EAConstants:
     VISUALIZER = False
     # Termination Criterion
     TERMINATION_CRITERION = StoppingByEvaluations
+    # Mutation
+    MUTATION = ReactorMutation
+    # Mutation Probability
+    MUTATION_PROBABILITY = 1.0
+    # Crossover
+    CROSSOVER = ReactorPseudoCrossover
+    # Crossover probability
+    CROSSOVER_PROBABILITY = 1.0
 
 
 class SAConstants:
     """
     Class containing a set of Simulated Annealing parameters constants
     """
-
-    MUTATION = ReactorMutation
-    MUTATION_PROBABILITY = 1
     TEMPERATURE = 1.0
     MINIMUM_TEMPERATURE = 0.000001
     ALPHA = 0.95
@@ -39,21 +44,27 @@ class GAConstants:
     """
     Class containing a set of Genetic Algorithm parameters constants
     """
-    MUTATION = ReactorMutation
-    MUTATION_PROBABILITY = 1
-    CROSSOVER = ReactorPseudoCrossover
-    CROSSOVER_PROBABILITY = 1
     SELECTION = BinaryTournamentSelection
+
+
+class ESConstants:
+    """
+    Class containing a set of Evolutionary Strategy parameters constants
+    """
+    ELITIST = False
+
+
+class LSConstants:
+    """
+    Class containing a set of Local Search parameters constants
+    """
+    COMPARATOR = DominanceComparator
 
 
 class NSGAIIIConstants:
     """
     Class containing a set of NSGAIII parameters constants
     """
-    MUTATION = ReactorMutation
-    MUTATION_PROBABILITY = 1
-    CROSSOVER = ReactorPseudoCrossover
-    CROSSOVER_PROBABILITY = 1
     REFERENCE_DIRECTIONS = UniformReferenceDirectionFactory
 
 
@@ -64,6 +75,13 @@ class GDE3Constants:
     CR = 0.5
     F = 0.5
     K = 0.5
+
+
+class IBEAConstants:
+    """
+    Class containing a set of IBEA parameters constants
+    """
+    KAPPA = 1.0
 
 
 class ChemConstants:
