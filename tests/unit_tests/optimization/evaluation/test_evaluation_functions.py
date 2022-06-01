ed4de8c295e765dcecb835f329b3921f8411613d
João Correia <jfscorreia95@gmail.com>
2022-06-01 14:28:30 +0100

    [ADD] base test case for evaluation functions

---
+++ tests/unit_tests/optimization/evaluation/test_evaluation_functions.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+from rdkit.Chem import MolFromSmiles
+
+
+class EvaluationFunctionBaseTestCase(ABC):
+
+    def setUp(self) -> None:
+        smiles = ['CCCCCCCCCCCCCCCCCC(=O)OC(CO)COP(=O)(O)OCCN', 'CCCCCCCCCCCCCC(=O)OC(CO)COP(=O)(O)OCC(O)CO',
+                  'O=C(O)C(=O)CC(O)C(O)COP(=O)(O)O', 'C=CCC(=O)C(=O)O',
+                  'COC(=O)C(O)C(OC1OC(CO)C(O)C(O)C1O)C(O)C(CO)OC(C)=O']
+        self.mols = [MolFromSmiles(smile) for smile in smiles]
+
+        self.mols_w_invalid = self.mols + [None]
+
+    @abstractmethod
+    def test_evaluation_function(self):
+        pass
+
+    @abstractmethod
+    def test_evaluation_function_with_invalid_mols(self):
+        pass
