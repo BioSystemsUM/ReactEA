0e399132b64e60a2125390731586cc3507c32eb8
João Correia <jfscorreia95@gmail.com>
2022-06-01 15:24:47 +0100

    [ADD] docstrings added

--- src/reactea/optimization/comparators.py
+++ src/reactea/optimization/comparators.py
@@ -1,13 +1,34 @@
 from .solution import ChemicalSolution, dominance_test
 
 
-# TODO: docstrings
 class ParetoDominanceComparator:
-    """"""
+    """
+    Compares two solutions using aggregate constraint violation and the Pareto dominance relation as originally
+    proposed by Kalyanmoy Deb.
+
+    References:
+        Deb, K., "An Efficient Constraint Handling Method for Genetic Algorithms." Computer Methods in Applied
+        Mechanics and Engineering, pp. 311--338, 1998.
+    """
 
     @staticmethod
     def compare(solution1: ChemicalSolution, solution2: ChemicalSolution):
-        """"""
+        """
+        Compares two solutions using the Pareto dominance comparator.
+        Parameters
+        ----------
+        solution1: ChemicalSolution
+            The first solution to compare.
+        solution2: ChemicalSolution
+            The second solution to compare.
+
+        Returns
+        -------
+        int:
+            -1 if solution1 is dominated by solution2,
+            0 if solution1 and solution2 are non-dominated,
+            1 if solution2 is dominated by solution1.
+        """
         if solution1 is None:
             raise Exception("The solution1 is None")
         elif solution2 is None:
