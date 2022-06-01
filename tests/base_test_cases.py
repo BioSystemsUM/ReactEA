245b035e3bdb45361cbfd482252059344d9c8dc1
João Correia <jfscorreia95@gmail.com>
2022-06-01 17:43:11 +0100

    [ADD] integration test for SweetReactor case study

---
+++ tests/base_test_cases.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+from abc import ABC, abstractmethod
+
+from rdkit import RDLogger
+
+from _utils import load_config, ROOT_DIR, SOURCE_DIR
+
+
+class CaseStudiesBaseTestCase(ABC):
+
+    def setUp(self):
+        # Mute RDKit logs
+        RDLogger.DisableLog("rdApp.*")
+
+        data_folder = os.path.join(ROOT_DIR, 'data')
+        config_path = os.path.join(data_folder, 'base_config.yaml')
+        self.configs = load_config(config_path)
+        self.output_folder = f"{SOURCE_DIR}/src/reactea/outputs/{self.configs['exp_name']}/"
+
+    def tearDown(self):
+        if os.path.exists(self.output_folder):
+            shutil.rmtree(self.output_folder)
+
+    @abstractmethod
+    def test_case_study(self):
+        pass
