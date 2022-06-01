63d3aced0a43b3e92e5e28c81eea37e028dbb169
João Correia <jfscorreia95@gmail.com>
2022-06-01 14:29:02 +0100

    [ADD] tests for Caloric evaluation function

---
+++ tests/unit_tests/optimization/evaluation/test_caloric.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from reactea.optimization.evaluation import Caloric
+from .test_evaluation_functions import EvaluationFunctionBaseTestCase
+
+
+class TestCaloric(EvaluationFunctionBaseTestCase, TestCase):
+
+    def test_evaluation_function(self):
+        mols = self.mols[-1]
+        spds = Caloric(maximize=True, worst_fitness=0.0)
+        scores = spds.get_fitness(mols)
+
+        self.assertEqual(len(scores), 1)
+        self.assertEqual(spds.method_str(), "Caloric")
+
+    def test_evaluation_function_with_invalid_mols(self):
+        mols = self.mols_w_invalid
+        spds = Caloric(maximize=True, worst_fitness=0.0)
+        scores = spds.get_fitness(mols)
+
+        self.assertEqual(len(scores), len(mols))
+        self.assertEqual(scores[-1], spds.worst_fitness)
