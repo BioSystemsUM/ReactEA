7c7e3c8b1d05f123c098a0023c335b55b84a3478
João Correia <jfscorreia95@gmail.com>
2022-06-15 14:45:58 +0100

    [ADD] AlgorithmsBaseTestCase base test for EA algorithms

--- tests/base_test_cases.py
+++ tests/base_test_cases.py
@@ -25,3 +25,23 @@ class CaseStudiesBaseTestCase(ABC):
     @abstractmethod
     def test_case_study(self):
         pass
+
+
+class AlgorithmsBaseTestCase(ABC):
+
+    def setUp(self):
+        # Mute RDKit logs
+        RDLogger.DisableLog("rdApp.*")
+
+        data_folder = os.path.join(ROOT_DIR, 'data')
+        config_path = os.path.join(data_folder, 'base_config.yaml')
+        self.configs = load_config(config_path)
+        self.output_folder = f"{SOURCE_DIR}/src/reactea/outputs/{self.configs['exp_name']}/"
+
+    def tearDown(self):
+        if os.path.exists(self.output_folder):
+            shutil.rmtree(self.output_folder)
+
+    @abstractmethod
+    def test_algorithms(self):
+        pass
