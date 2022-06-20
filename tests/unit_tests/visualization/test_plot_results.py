94047faef36475b4f7dcfe2a8d831870239a2d04
João Correia <jfscorreia95@gmail.com>
2022-06-20 18:49:57 +0100

    [ADD] PlotResults tests

---
+++ tests/unit_tests/visualization/test_plot_results.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from reactea.vizualization.plot_results import PlotResults
+
+
+class TestPlotResults(TestCase):
+
+    def test_plot_results(self):
+        mock_output_configs = {'transformations_path': '/home/jcorreia/PycharmProjects/BioSystemsUM/ReactEA/src/'
+                                                       'reactea/outputs/GA_rr_600esc_400gen_0.6sweet_0.4_caloric/'
+                                                       'FINAL_TRANSFORMATIONS_04-29_16-51-54.csv'}
+        PlotResults(mock_output_configs, solution_index=0).plot_results(save_fig=False)
+        PlotResults(mock_output_configs, solution_index=4).plot_results(save_fig=False)
+        PlotResults(mock_output_configs, solution_index=6).plot_results(save_fig=False)
