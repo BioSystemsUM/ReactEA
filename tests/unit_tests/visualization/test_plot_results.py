e335d73440e1c849f1cc7c0381300f612835178c
João Correia <jfscorreia95@gmail.com>
2022-06-23 17:22:25 +0100

    [REFACTOR] change tests to the new multiprocessing formulation

--- tests/unit_tests/visualization/test_plot_results.py
+++ tests/unit_tests/visualization/test_plot_results.py
@@ -7,7 +7,8 @@ class TestPlotResults(TestCase):
 
     def test_plot_results(self):
         mock_output_configs = {'transformations_path': '/home/jcorreia/PycharmProjects/BioSystemsUM/ReactEA/src/'
-                                                       'reactea/outputs/GA_rr_600esc_400gen_0.6sweet_0.4_caloric/'
+                                                       'reactea/outputs/old_results/'
+                                                       'GA_rr_600esc_400gen_0.6sweet_0.4_caloric/'
                                                        'FINAL_TRANSFORMATIONS_04-29_16-51-54.csv'}
         PlotResults(mock_output_configs, solution_index=0).plot_results(save_fig=False)
         PlotResults(mock_output_configs, solution_index=4).plot_results(save_fig=False)
