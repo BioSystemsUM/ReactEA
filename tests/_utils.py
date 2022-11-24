fea89d031c87bea8c75a7fa92d891d844f57b672
João Correia <jfscorreia95@gmail.com>
2022-11-24 09:54:54 +0000

    [REFACTORING] static set of rules, remove methods for loading mutable sets of rules

--- tests/_utils.py
+++ tests/_utils.py
@@ -5,7 +5,6 @@ import pandas as pd
 import yaml
 
 from reactea.chem.compounds import Compound
-from reactea.chem.reaction_rules import ReactionRule
 from reactea.utilities.constants import ChemConstants
 
 ROOT_DIR = os.path.dirname(__file__)
@@ -68,50 +67,8 @@ def load_initial_population_smiles(configs: dict):
     Returns
     -------
     List[str]:
-        list of compound' smiles used as initial population
+        list of compounds' smiles used as initial population
     """
     cmp_df = pd.read_csv(from_root(configs["init_pop_path"]), header=0, sep='\t')
     cmp_df = cmp_df.sample(configs["init_pop_size"])
     return cmp_df.smiles.values
-
-
-def initialize_rules(configs: dict):
-    """
-    Loads the reaction rules.
-
-    Parameters
-    ----------
-    configs: dict
-        configurations of the experiment (containing path to reaction rules file)
-
-    Returns
-    -------
-    List[ReactionRule]:
-        list of reaction rules to use
-    """
-    rules_df = pd.read_csv(from_root(configs["rules_path"]), header=0, sep='\t')
-    if configs["use_coreactant_info"]:
-        coreactants = initialize_coreactants(configs)
-        return [ReactionRule(row['smarts'],
-                             row["rule_id"], row["coreactants_ids"]) for _, row in rules_df.iterrows()], coreactants
-    else:
-        return [ReactionRule(row['smarts'], row["rule_id"]) for _, row in rules_df.iterrows()], None
-
-
-def initialize_coreactants(configs: dict):
-    """
-    Loads the set of coreactants
-
-    Parameters
-    ----------
-    configs: dict
-        configurations of the experiment (containing path to coreactants file)
-
-    Returns
-    -------
-    List[Compound]:
-        list of compounds to use as coreactants
-    """
-    coreactants_df = pd.read_csv(from_root(configs["coreactants_path"]), header=0, set='\t')
-    return [ChemConstants.STANDARDIZER().standardize(
-        Compound(row['smiles'], row["compound_id"])) for _, row in coreactants_df.iterrows()]
