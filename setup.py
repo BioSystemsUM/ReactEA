a5eab86a466a21b579c64b727766e702915ea95a
João Correia <jfscorreia95@gmail.com>
2022-11-25 12:17:17 +0000

    [ADD] CI workflow + [UPDATE] setups

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-setup(
-    name='reactea',
-    version='0.0.0',
-    python_requires='>=3.6',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    include_package_data=True,
-    zip_safe=False,
-    author='BiSBII CEB University of Minho',
-    author_email='jcorreia95@gmail.com',
-    description='...',
-    license='MIT',
-    keywords='...',
-    url='...',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-)
+if __name__ == "__main__":
+    setup()
