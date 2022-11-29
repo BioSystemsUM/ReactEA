1803f75a9f17bc9243f7a7c1759494fe51156706
João Correia <jfscorreia95@gmail.com>
2022-11-29 10:18:53 +0000

    [FIX] data path

--- tests/unit_tests/visualization/test_plot_results.py
+++ tests/unit_tests/visualization/test_plot_results.py
@@ -1,15 +1,15 @@
+import os.path
 from unittest import TestCase
 
 from reactea.vizualization.plot_results import PlotResults
+
+from tests import TEST_DIR
 
 
 class TestPlotResults(TestCase):
 
     def test_plot_results(self):
-        mock_output_configs = {'transformations_path': '/home/jcorreia/PycharmProjects/BioSystemsUM/ReactEA/src/'
-                                                       'reactea/outputs/old_results/'
-                                                       'GA_rr_600esc_400gen_0.6sweet_0.4_caloric/'
-                                                       'FINAL_TRANSFORMATIONS_04-29_16-51-54.csv'}
+        mock_output_configs = {'transformations_path': os.path.join(TEST_DIR, 'data/output_example/GA_rr_600esc_400gen_0.6sweet_0.4_caloric/FINAL_TRANSFORMATIONS_04-29_16-51-54.csv')}
         PlotResults(mock_output_configs, solution_index=0).plot_results(save_fig=False)
         PlotResults(mock_output_configs, solution_index=4).plot_results(save_fig=False)
         PlotResults(mock_output_configs, solution_index=6).plot_results(save_fig=False)
