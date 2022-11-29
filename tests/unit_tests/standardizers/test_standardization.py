1bbdf70523ff8e6b5e81a2fd92c3d189e51c49e2
João Correia <jfscorreia95@gmail.com>
2022-11-29 10:02:44 +0000

    [FIX] remove abstract test classes

--- tests/unit_tests/standardizers/test_standardization.py
+++ tests/unit_tests/standardizers/test_standardization.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from unittest import TestCase
 
 from rdkit.Chem import Mol
@@ -7,22 +6,13 @@ from reactea.chem import Compound
 from reactea.standardizers import ChEMBLStandardizer
 
 
-class TestMolecularStandardizer(ABC):
+class TestChEMBLStandardizer(TestCase):
 
-    def setUp(self):
+    def test_molecular_standardizer(self):
         smiles = [('CCCCCCCCCCCCCCCCCC(=O)OC(CO)COP(=O)(O)OCCN', 'id1'), ('CC)(CC=', 'id3')]
-        self.compounds = [Compound(sm[0], sm[1]) for sm in smiles]
-
-    @abstractmethod
-    def test_molecular_standardizer(self):
-        pass
-
-
-class TestChEMBLStandardizer(TestMolecularStandardizer, TestCase):
-
-    def test_molecular_standardizer(self):
-        cmp1 = ChEMBLStandardizer().standardize(self.compounds[0])
-        cmp2 = ChEMBLStandardizer().standardize(self.compounds[1])
+        compounds = [Compound(sm[0], sm[1]) for sm in smiles]
+        cmp1 = ChEMBLStandardizer().standardize(compounds[0])
+        cmp2 = ChEMBLStandardizer().standardize(compounds[1])
         self.assertIsInstance(cmp1, Compound)
         self.assertIsInstance(cmp1.smiles, str)
         self.assertIsInstance(cmp1.mol, Mol)
